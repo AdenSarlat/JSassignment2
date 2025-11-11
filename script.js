9e9e137db9f06fc95f6306053728a16f9b9776ab
Aden Sarlat <200608715@student.georgianc.on.ca>
2025-11-11 13:26:27 -0500

    added smoothie pricing data for everthing and began js

---
+++ script.js
@@ -0,0 +1,28 @@
+const prices = {
+  size: {
+    Small: 5.00,
+    Medium: 5.59,
+    Large: 7.90
+  },
+  ingredients: {
+    Banana: 0.50,
+    Strawberry: 0.75,
+    Blueberry: 0.45,
+    Mango: 1.00,
+    Rasperry: 0.90,
+    Pineapple: 0.90,
+    Coconut: 0.20
+  },
+  base: {
+    Water: 0.00,
+    "Almond Milk": 0.50,
+    Milk: 0.70,
+    Yogurt: 0.95
+  },
+  sweetener: {
+    None: 0.00,
+    Honey: 0.30,
+    "Sugar Cane": 0.25,
+    Sugar: 0.20
+  }
+};
